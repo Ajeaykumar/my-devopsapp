ba35d4751e29a48099c9a8a67830af6021da914b
Ajeaykumar <ajeaybastann7999@gmail.com>
2025-11-07 20:47:37 +0530

    updated app to version 2.0

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return "Hello DevOps World! This is version 1.0!"
+  return "Hello DevOps World here is your Boss! This is version 2.0!"
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
